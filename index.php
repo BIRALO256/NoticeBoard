af3e4669faf7a0982591445fc381f948446e013a
DallingtonAsin <asingwire50dallington@gmail.com>
2022-01-18 16:08:29 +0300

    removed unnecessary files

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <html>
 <head> 
 	<meta="viewport" content="width=device-width, initial-scale=1">
-	<title>notic
+	<title>notice board</title>
 	<meta charset="utf-8">
-	<meta name board</title>
 	<link rel="stylesheet" type="text/css" href="login.css">
 	<script type="text/javascript" src="validation.js" ></script>
 </head>
